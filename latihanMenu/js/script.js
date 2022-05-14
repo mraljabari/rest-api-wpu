1d6371d1f9857bc14bc7d612922a07f0dd7839b0
mraljabri <hydeki.rizky@gmail.com>
2022-05-14 22:18:48 +0700

    update latihan json menu pizza

--- latihanMenu/js/script.js
+++ latihanMenu/js/script.js
@@ -2,6 +2,16 @@ $.getJSON(`data/pizza.json`, function(data){
     let menu = data.menu
     // console.log(menu);
     $.each(menu, function(i, data){
-        console.log(i);
+        $('#daftar-menu').append(`<div class="col-md-4">
+        <div class="card mb-3">
+            <img src="img/menu/`+ data.gambar +`" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">`+ data.nama +`</h5>
+                <p class="card-text">`+ data.deskripsi +`</p>
+                <p class="card-text"><b>Harga : Rp. `+ data.harga +`</b></p>
+                <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+            </div>
+        </div>`)
     });
 });
